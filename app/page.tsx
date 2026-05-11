c3dd397236f3682bfa6b03b4f1426420a0ec3eca
moralesandersonr <moralesandersonr@gmail.com>
2026-05-11 11:33:47 -0500

    test deploy fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,3 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import Footer from "./components/Footer";
-import WhatsAppButton from "./components/WhatsAppButton";
-
 export default function Home() {
-  return (
-    <main className="min-h-screen flex flex-col">
-      
-      <Navbar />
-
-      <Hero />
-
-      <About />
-
-      <Projects />
-
-      <Footer />
-
-      <WhatsAppButton />
-
-    </main>
-  );
+  return <h1>TEST OK</h1>;
 }
