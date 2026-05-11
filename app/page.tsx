c1dc6701e240f0ddbd6e6e01041e181906126f70
moralesandersonr <moralesandersonr@gmail.com>
2026-05-11 12:06:11 -0500

    clean build fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,27 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Footer from "./components/Footer";
+import WhatsAppButton from "./components/WhatsAppButton";
+
 export default function Home() {
-  return <h1>TEST OK</h1>;
+  return (
+    <main className="min-h-screen flex flex-col">
+      
+      <Navbar />
+
+      <Hero />
+
+      <About />
+
+      <Projects />
+
+      <Footer />
+
+      <WhatsAppButton />
+
+    </main>
+  );
 }
+  
